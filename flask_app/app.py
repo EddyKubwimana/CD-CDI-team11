2c76857875fe6de946be880636f5b956ecf36b15
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2024-11-01 17:22:23 +0000

    updating the page

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,64 +1,35 @@
 from flask import Flask, render_template, request, jsonify
-import redis
-import mysql.connector
 
 app = Flask(__name__)
 
-# Redis for tracking page visits
-r = redis.Redis(host="redis", port=6379)
-
 @app.route("/")
 def home():
-    count = r.incr("hits")
-    conn = db_connection("mysql", "EddyKubwimana", "password", "cloud")
-    cursor = conn.cursor()
+    '''
+    Landing page
 
-    # Create the table if not exists
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS people (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        firstname VARCHAR(100),
-        lastname VARCHAR(100),
-        registration_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-    ''')
+    '''
 
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT * FROM people ORDER BY registration_time DESC')
-    people_data = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    return render_template("home.html")
 
-    return render_template("index.html", count=count, people=people_data)
+@app.route('/tinotenda')
+def tinotenda_portfolio():
+    return render_template('tinotenda.html')
 
+@app.route('/nicholas')
+def nicholas_portfolio():
+    return render_template('nicholas.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
-    conn = db_connection("mysql", "EddyKubwimana", "password", "cloud")
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO people (firstname, lastname) VALUES (%s, %s)', (firstname, lastname))
-    conn.commit()
+@app.route('/candace')
+def candace_portfolio():
+    return render_template('candace.html')
 
-    cursor.execute('SELECT * FROM people ORDER BY registration_time DESC')
-    people_data = cursor.fetchall()
-    cursor.close()
-    conn.close()
-
-   
-    return jsonify({'message': 'User added successfully!', 'people': people_data, 'count': r.get("hits").decode()})
+@app.route('/eddy')
+def eddy_portfolio():
+    return render_template('eddy.html')
 
 
-def db_connection(host, username, password, database):
-    connection = mysql.connector.connect(
-        host=host,
-        user=username,
-        password=password,
-        database=database
-    )
-    return connection
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
