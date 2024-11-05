f2e14bacf9eff3127c18f88a227332eef2bd162d
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-11-05 12:04:23 +0000

    test pipeline

--- porfolio/app.py
+++ porfolio/app.py
@@ -2,34 +2,41 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    '''
+    """
     Landing page
 
-    '''
+    """
 
     return render_template("home.html")
 
-@app.route('/tinotenda')
+
+@app.route("/tinotenda")
 def tinotenda_portfolio():
-    return render_template('tinotenda.html')
+    return render_template("tinotenda.html")
 
-@app.route('/nicholas')
+
+@app.route("/nicholas")
 def nicholas_portfolio():
-    return render_template('nicholas.html')
+    return render_template("nicholas.html")
 
-@app.route('/candace')
+
+@app.route("/candace")
 def candace_portfolio():
-    return render_template('candace.html')
+    return render_template("candace.html")
 
-@app.route('/eddy')
+
+@app.route("/eddy")
 def eddy_portfolio():
-    return render_template('eddy.html')
+    return render_template("eddy.html")
 
 
+@app.route("/oheneba")
+def oheneba_portfolio():
+    return render_template("oheneba.html")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
